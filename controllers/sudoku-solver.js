25fd5b438c2bfdf52379a493ffd8a1cbddf96e16
Garik H <garik.hakobyan.2004@gmail.com>
2023-04-10 21:42:36 +0300

    feat: init-ed nodemodules and added some routes

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,28 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    const regex = /^[1-9\.]+$/;
+
+    if (puzzleString.length !== 81) {
+      return { error: 'Expected puzzle to be 81 characters long' };
+    }
+
+    if (!regex.test(puzzleString)) {
+      return { error: 'Invalid characters in puzzle' };
+    }
+    return false;
+  }
+
+  isValidCoordinate(coordinate) {
+    const regex = /^[A-I][1-9]$/;
+
+    if (!regex.test(coordinate)) {
+      return false;
+    }
+
+    const row = coordinate.charCodeAt(0) - 65 + 1;
+    const col = parseInt(coordinate.charAt(1));
+    return { row, col };
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
@@ -16,7 +38,104 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
-    
+    const board = puzzleString.replace(/\./g, 0).split('').map(i => parseInt(i));
+
+    // index -> { row, col }
+    function i2rc(index) {
+      return { row: Math.floor(index / 9), col: index % 9 };
+    }
+
+    // { row, col } -> index
+    function rc2i(row, col) {
+      return row * 9 + col;
+    }
+
+    function getChoices(board, index) {
+      let choices = [];
+      for (let value = 1; value <= 9; ++value) {
+        if (acceptable(board, index, value)) {
+          if (unique(board, index, value))
+            return [value]; // it's useless to try anything else
+          else
+            choices.push(value);
+        }
+      }
+      return choices;
+    }
+
+    function unique(board, index, value) {
+      let { row, col } = i2rc(index);
+      let r1 = Math.floor(row / 3) * 3;
+      let c1 = Math.floor(col / 3) * 3;
+      for (let r = r1; r < r1 + 3; ++r) {
+        for (let c = c1; c < c1 + 3; ++c) {
+          let i = rc2i(r, c);
+          if (i != index && !board[i] && acceptable(board, i, value)) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+    function acceptable(board, index, value) {
+      let { row, col } = i2rc(index);
+
+      // if already present on the column, not acceptable
+      for (let r = 0; r < 9; ++r)
+        if (board[rc2i(r, col)] == value) return false;
+
+      // if already present on the row, not acceptable
+      for (let c = 0; c < 9; ++c)
+        if (board[rc2i(row, c)] == value) return false;
+
+      // if already present in the same 3x3 square, also not acceptable
+      let r1 = Math.floor(row / 3) * 3;
+      let c1 = Math.floor(col / 3) * 3;
+      for (let r = r1; r < r1 + 3; ++r) {
+        for (let c = c1; c < c1 + 3; ++c) {
+          if (board[rc2i(r, c)] == value) return false;
+        }
+      }
+
+      return true;
+    }
+
+    function bestBet(board) {
+      let index, moves, bestLen = 100;
+      for (let i = 0; i < 81; ++i) {
+        if (!board[i]) {
+          let m = getChoices(board, i);
+          if (m.length < bestLen) {
+            bestLen = m.length;
+            moves = m;
+            index = i;
+            if (bestLen == 0) break;
+          }
+        }
+      }
+      return { index, moves };
+    }
+
+    function puzzleSolve() {
+      let { index, moves } = bestBet(board);    // find the best place to fill
+      if (index == null) return true;           // we filled'em all, success!
+      for (let m of moves) {
+        board[index] = m;                       // try one choice
+        if (puzzleSolve()) return true;         // if we solved further, success!
+      }
+      board[index] = 0;                         // no digit fits here, backtrack!
+      return false;
+    }
+
+    console.time();
+    const solved = puzzleSolve();
+    console.timeEnd();                                 //...8.1..........435............7.8........1...2..3....6......75..34........2..6..
+    if (solved) {
+      return board.join('');
+    } else {
+      return false;
+    }
   }
 }
 
--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,36 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
+
   let solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
+      const { puzzle, coordinate, value} = req.body;
+      if (!puzzle || !coordinate || !value) return res.json({ error: 'Required field(s) missing' });
 
+      if (!/^[1-9]$/.test(value)) return res.json({ error: 'Invalid value' });
+
+      const valid = solver.validate(puzzle);
+      if (valid.hasOwnProperty('error')) return res.status(400).json(valid);
+
+      const validCoordinate = solver.isValidCoordinate(coordinate);
+      if (!validCoordinate) return res.json({ error: 'Invalid coordinate' });
+      const { row, col } = validCoordinate;
+      
+      console.log( row, col );
     });
-    
+
   app.route('/api/solve')
     .post((req, res) => {
+      const puzzle = req.body.puzzle;
+      if (!puzzle) return res.status(400).json({ error: 'Required field missing' });
 
+      const valid = solver.validate(puzzle);
+      if (valid.hasOwnProperty('error')) return res.status(400).json(valid);
+
+      const solution = solver.solve(puzzle);
+      if (!solution) return res.status(400).json({ error: 'Puzzle cannot be solved' });
+      return res.status(200).json({ solution });
     });
 };
